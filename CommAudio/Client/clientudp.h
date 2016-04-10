9897cc51f335ba4b1e83ffb10a26a27be7d203af
jerrybeanman <jerry1bean@hotmail.com>
2016-04-10 15:05:33 -0700

    added send function for client

--- CommAudio/Client/clientudp.h
+++ CommAudio/Client/clientudp.h
@@ -7,6 +7,7 @@ class ClientUDP : public Client
 {
     public:
         bool InitializeSocket(short port);
+        bool InitializeSendingSocket(char * ip, short port);
 
         bool MulticastSettings(const char * name);
 
@@ -20,6 +21,7 @@ class ClientUDP : public Client
 
     private:
         struct ip_mreq      MulticastInfo;
+        SOCKET_INFORMATION  SendingSocketInfo;
         SOCKADDR_IN         SourceAddress;
         DWORD               Flags = 0;
         DWORD               SourceLen = sizeof(SourceAddress);
