ce644cc193bfb415529b23f0279a94c140fb8904
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-04-12 23:08:59 -0700

    Test client

--- CommAudio/globals.h
+++ CommAudio/globals.h
@@ -11,7 +11,7 @@
 #define DEAULT_MULTICAST_IP  "234.5.6.7"
 #define MAX_CLIENTS          10
 #define DATA_BUFSIZE         40000
-#define DEFAULT_PORT         7000
+#define DEFAULT_PORT         7050
 #define TCP_PORT             8000
 #define UpdateList           "UpdaL:"
 #define FileBegin            "FileS:"
